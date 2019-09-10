0d351285a618216d17a0ae272b2d7fbeff162de5
Kisanet Gebregergis <54561006+kisanet-Gebregergis@users.noreply.github.com>
2019-09-09 20:16:48 -0400

    Add files via upload

---
+++ LoadingDatafromExcel.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd 
+Location = "CRM01.xls"
+df = pd.read_excel(Location)
+
+
+# In[2]:
+
+
+df.head()
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
