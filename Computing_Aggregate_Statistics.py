83daff36371be305454ca14f93cfa7f6c839d4d0
Kisanet Gebregergis <54561006+kisanet-Gebregergis@users.noreply.github.com>
2019-10-07 22:08:07 -0400

    Add files via upload

---
+++ Computing_Aggregate_Statistics.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd
+
+names = ['Bob','Jessica','Mary','John','Mel']
+grades = [76,95,77,78,99]
+bsdegrees = [1,1,0,0,1]
+msdegrees = [2,1,0,0,0]
+phddegrees = [0,1,0,0,0]
+
+
+# In[2]:
+
+
+GradeList = zip(names,grades,bsdegrees,msdegrees,phddegrees)
+df = pd.DataFrame(data = GradeList, columns=['Names', 'Grades','BS','MS','PHD'])
+df
+
+
+# In[3]:
+
+
+df.count() #computes the number of values
+
+
+# In[4]:
+
+
+df.mean()
+
+
+# In[5]:
+
+
+df.std()
+
+
+# In[6]:
+
+
+df.max()
+
+
+# In[7]:
+
+
+df.min()
+
+
+# In[8]:
+
+
+df.quantile(.25)
+
+
+# In[9]:
+
+
+df.quantile(.5)
+
+
+# In[10]:
+
+
+df.quantile(.75)
+
+
+# In[ ]:
+
+
+
+
