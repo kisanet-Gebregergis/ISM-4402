8f3619988eb95f239da2c16b98ee65f50c900d08
Kisanet Gebregergis <54561006+kisanet-Gebregergis@users.noreply.github.com>
2019-10-14 02:16:29 -0400

    Add files via upload

---
+++ Sorting_Data.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd
+
+Location = "datasets/gradedata.csv"
+df = pd.read_csv(Location)
+
+df.head()
+
+
+# In[2]:
+
+
+df = df.sort_values(by=['fname', 'age', 'grade'], ascending=[True, True, False])
+df.head()
+
+
+# In[3]:
+
+
+df = df.sort_values(by=['lname', 'age', 'grade'], ascending=[True, True, True])
+df.head()
+
+
+# In[ ]:
+
+
+
+
